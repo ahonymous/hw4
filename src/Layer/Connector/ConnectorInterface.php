6cc5918a42c9e1af6adf7eb0b5ff3ce405ad0d75
Victor <viktornord1@gmail.com>
2015-10-29 00:59:00 +0200

    connection to db

--- src/Layer/Connector/ConnectorInterface.php
+++ src/Layer/Connector/ConnectorInterface.php
@@ -14,7 +14,7 @@ interface ConnectorInterface
      * @param $password
      * @return mixed
      */
-    public function connect($host, $user, $password);
+    public function connect($host, $dbName, $user, $password);
 
     /**
      * @param $db
--- web/index.php
+++ web/index.php
@@ -1,5 +1,16 @@
 <?php
-
+ini_set('display_errors', 'on');
 require __DIR__ . '/../config/autoload.php';
+use Layer\Connector\Connector;
+$connector = new Connector();
+$conn = $connector->connect(
+    $GLOBALS['config']['host'],
+    $GLOBALS['config']['db_name'],
+    $GLOBALS['config']['db_user'],
+    $GLOBALS['config']['db_password']
+);
+$sql = "SELECT FIRST_NAME FROM USER";
 
-echo "Hi ALL!";
+foreach ($conn->query($sql) as $row) {
+    print $row['FIRST_NAME'] . "\t";
+}
