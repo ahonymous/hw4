b22f18f60e01d3d90ef745ca62c0739de23fb69b
Maxim Zhurilo <fumus.27@gmail.com>
2015-10-29 13:40:25 +0200

    added new entity Customer

---
+++ src/Entity/Customer.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fumus
+ * Date: 29.10.15
+ * Time: 13:26
+ */
+
+namespace Entity;
+
+
+class Customer
+{
+    protected $firstName;
+    protected $lastName;
+    protected $email;
+
+    public function __construct($firstName, $lastName, $email)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->email = $email;
+    }
+
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    
+}
