fe75b644ced6da62be2ec52878d7dd2c06b7f49c
Maxim Zhurilo <fumus.27@gmail.com>
2015-10-30 17:18:36 +0200

    updated CustomersController, added method editAction

--- src/Controllers/CustomersController.php
+++ src/Controllers/CustomersController.php
@@ -37,7 +37,7 @@ class CustomersController
         if (isset($_POST['first_name'])) {
             $customer = new Customer($_POST['first_name'], $_POST['last_name'], $_POST['email']);
             $this->manager->insert($customer);
-            
+
             return $this->indexAction();
         }
         return $this->twig->render('customers_form.html.twig',
@@ -45,6 +45,25 @@ class CustomersController
                 'first_name' => '',
                 'last_name' => '',
                 'email' => '',
+            ]
+        );
+    }
+
+    public function editAction()
+    {
+        if (isset($_POST['first_name'])) {
+            $customer = new Customer($_POST['first_name'], $_POST['last_name'], $_POST['email']);
+            $this->manager->update((int) $_GET['id'], $customer);
+
+            return $this->indexAction();
+        }
+        $customersData = $this->manager->find((int) $_GET['id'])[0];
+        return $this->twig->render('customer_edit.html.twig',
+            [
+                'first_name' => $customersData['firstName'],
+                'last_name' => $customersData['lastName'],
+                'email' => $customersData['email'],
+                'id' => $_GET['id']
             ]
         );
     }
