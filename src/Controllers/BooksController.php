0056135c5f730dc99bbd1050c6896d67fb79526b
Maxim Zhurilo <fumus.27@gmail.com>
2015-10-30 22:00:09 +0200

    updated BooksController, added editAction

--- src/Controllers/BooksController.php
+++ src/Controllers/BooksController.php
@@ -57,4 +57,32 @@ class BooksController
             ]
         );
     }
+
+    public function editAction()
+    {
+        if (isset($_POST['title'])) {
+            $book = new Book();
+            $book->setTitle($_POST['title']);
+            $book->setAuthor($_POST['author']);
+            $book->setIsbn($_POST['isbn']);
+            $book->setPublishingHouse($_POST['publishing_house']);
+            $book->setPrice($_POST['price']);
+
+            $this->manager->update((int) $_GET['id'], $book);
+
+            return $this->indexAction();
+        }
+        $booksData = $this->manager->find((int) $_GET['id'])[0];
+
+        return $this->twig->render('books_edit.html.twig',
+            [
+                'title' => $booksData['title'],
+                'author' => $booksData['author'],
+                'isbn' => $booksData['isbn'],
+                'publishing_house' => $booksData['publishingHouse'],
+                'price' => $booksData['price'],
+                'id' => $_GET['id']
+            ]
+        );
+    }
 }
