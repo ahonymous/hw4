b1647f6e0e38e77c632f2990fd52ac524f20cc00
DenisRupp <rupp1993@gmail.com>
2015-11-02 22:37:12 +0200

    Write controller for User and Customers

--- index.php
+++ index.php
@@ -6,25 +6,127 @@ use Models\User;
 use Models\Customer;
 
 if (isset($_GET['action'])){
+
     switch ($_GET['action']){
         case 'users':
-            if ($_GET['subaction']!=''){
+            if ($_GET['subaction']){
                 switch($_GET['subaction']){
+
                     case 'del':
                         $user = new User();
-                        $del = $user->remove($_GET);
+                        $del = $user->remove($_POST['id']);
+
                         if ($del){
                             echo 'Пользователь удален';
                         } else {
                             echo 'Возникла ошибка';
                         }
+
+                        break;
+
+                    case 'add':
+                        $user = new User();
+                        $addUser = $user->insert($_POST);
+
+                        if ($addUser){
+                            echo '<script type="text/javascript">';
+                            echo 'window.location.href="/index.php?action=users";';
+                            echo '</script>';
+                        } else {
+                            echo 'Ошибка при добавлении нового пользователя';
+                        }
+
+                    break;
+
+                    case 'find':
+                        $user = new User();
+                        $find = $user->find($_POST['colm'], $_POST['id']);
+
+                        $smarty->assign('user', $find);
+                        $smarty->display('serch_resul.tpl');
+
+                        break;
+
+                    default:
+
+                        $user = new User();
+                        $allUsers = $user->findAll();
+
+                        $smarty->assign('users',$allUsers);
+                        $smarty->display('users.tpl');
+
                         break;
                 }
             }
-            $smarty->display('users.tpl');
             break;
-        case 'managers':
-            $smarty->display('managers.tpl');
+        case 'customers':
+            if ($_GET['subaction']){
+                switch($_GET['subaction']){
+
+                    case 'del':
+                        $customer = new Customer();
+                        $del = $customer->remove($_POST['id']);
+
+                        if ($del){
+                            echo 'Продавец удален';
+                        } else {
+                            echo 'Возникла ошибка';
+                        }
+
+                        break;
+
+                    case 'add':
+                        $customer = new Customer();
+                        $addCustomer = $customer->insert($_POST);
+
+                        if ($addUser){
+                            echo '<script type="text/javascript">';
+                            echo 'window.location.href="/index.php?action=customers";';
+                            echo '</script>';
+                        } else {
+                            echo 'Ошибка при добавление нового продавца';
+                        }
+
+                        break;
+
+                    case 'find':
+                        $customer = new Customer();
+                        $find = $customer->find($_POST['colm'], $_POST['id']);
+
+                        $smarty->assign('custoner', $find);
+                        $smarty->display('serch_result_customer.tpl');
+
+                        break;
+
+                    default:
+
+                        $user = new User();
+                        $allUsers = $user->findAll();
+
+                        $smarty->assign('users',$allUsers);
+                        $smarty->display('users.tpl');
+
+                        break;
+                }
+            }
+            break;
+
+        case 'order':
+            if ($_GET['subaction']){
+                switch($_GET['subaction']) {
+                    case '':
+
+                        break;
+                }
+            } else {
+                $user = new User();
+                $allUsers = $user->findAll();
+
+                $customer = new Customer();
+                $allCustomers = $customer->findAll();
+            }
+
+
             break;
         case 'stuff':
             $smarty->display('stuff.tpl');
