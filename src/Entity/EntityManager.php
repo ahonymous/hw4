7a0838988e494972441219beb2bd0693e613d57b
Roma48 <myzfann@mail.ru>
2015-11-08 22:30:30 +0200

    merge conflicts

--- src/Entity/EntityManager.php
+++ src/Entity/EntityManager.php
@@ -1,53 +1,51 @@
 <?php
-
 namespace Entity;
-
 use Layer\Manager\AbstractManager;
-use Layer\Connector\ConnectorInterface;
-use PDO;
 use Layer\Connector\Connector;
-
+use User\User;
+use PDO;
 /**
  * Class EntityManager
  * @package Entity
  */
-class EntityManager extends AbstractManager implements ConnectorInterface
+class EntityManager extends AbstractManager
 {
     /**
      * @var PDO
      */
     private $connection;
-
     public function __construct()
     {
         $this->connection = new Connector();
         $this->connection = $this->connection->connect();
-
     }
-
     public function __destruct()
     {
         $this->connection = null;
     }
-
     public function db_init(){
-
         $query = $this->connection->query("CREATE TABLE `user` (id INT(11) AUTO_INCREMENT PRIMARY KEY, username VARCHAR(60), createdat INT(11), updatedat INT(11), deletedat INT(11) )");
         $query = $this->connection->query("CREATE TABLE `product` (id INT(11) AUTO_INCREMENT PRIMARY KEY, name VARCHAR(60), price INT(11), createdat INT(11), updatedat INT(11), deletedat INT(11) )");
         $query = $this->connection->query("CREATE TABLE `order` (id INT(11) AUTO_INCREMENT PRIMARY KEY, userid INT(11), productid INT(11), price INT(11))");
-
     }
-
-
     /**
-     * @param $db
-     * @return null
+     * @param Object $en
+     * @return array
      */
-    public function connectClose($db)
+    public function entityParse($en)
     {
-        return $this->connection = null;
+        $entityArray = [];
+        $entity = new \ReflectionClass($en);
+        $entityArray['entity'] = $entity->getShortName();
+        $entityProperties = $entity->getProperties();
+        foreach ($entityProperties as $property){
+            $getProperty = $entity->getProperty($property->name);
+            $getProperty->setAccessible(true);
+            $val = $getProperty->getValue($en);
+            $entityArray['properties'][$property->name] = $val;
+        }
+        return $entityArray;
     }
-
     /**
      * @param array $params
      * @return string
@@ -56,22 +54,16 @@ class EntityManager extends AbstractManager implements ConnectorInterface
     {
         $values = [];
         $fields = [];
-
         foreach ($params['properties'] as $key => $value){
             if ($value != '') {
                 $values[] = "'" . $value . "'";
                 $fields[] = "`" . $key . "`";
             }
         }
-
         $table = strtolower($params['entity']);
-
         $sql = sprintf("INSERT INTO %s (%s) VALUES (%s)", $table, implode($fields, ','), implode($values, ','));
-
         return $sql;
     }
-
-
     /**
      * @param array $entity
      * @return int
@@ -79,17 +71,11 @@ class EntityManager extends AbstractManager implements ConnectorInterface
     public function insert($entity)
     {
         $params = $this->entityParse($entity);
-
         $sql = $this->prepareSql($params);
-
         $query = $this->connection->prepare($sql);
-
         $query->execute();
-
         return (int)$this->connection->lastInsertId();
-
     }
-
     /**
      * @param $entity
      * @return \PDOStatement
@@ -103,44 +89,31 @@ class EntityManager extends AbstractManager implements ConnectorInterface
                 $values[strtolower(substr($method, 3))] = $entity->$method();
             }
         }
-
         $table = new \ReflectionClass($entity);
         $table = strtolower($table->getShortName());
-
         $update = '';
-
         foreach ($values as $key => $val){
             if ($val && $key != 'id') {
-                $update .= $key . ' = ' . $this->connection->quote($val) . ',';
+                $update .= $key . ' = ' . $connection->quote($val) . ',';
             }
         }
-
         $update = trim($update, ",");
-
         $id = $values['id'];
-
         $query = $this->connection->prepare("UPDATE $table SET $update WHERE id=$id");
-
         return $query->execute();
     }
-
     /**
      * @param $entity
      * @return \PDOStatement
      */
     public function remove($entity)
     {
-
         $table = $entity['entity'];
         $id = $entity['id'];
-
         $sql = "DELETE FROM `$table` WHERE `id`=" . $id;
         $result = $this->connection->query($sql);
-
         return $result;
-
     }
-
     /**
      * @param $entityName
      * @param $id
@@ -150,28 +123,22 @@ class EntityManager extends AbstractManager implements ConnectorInterface
     {
         $query = $this->connection->query("SELECT * FROM $entityName WHERE id=$id");
         $query->execute();
-
         return $query->fetch();
     }
-
     /**
      * @param $entityName
      * @return array
      */
     public function findAll($entityName)
     {
-
         if ($entityName == 'order'){
             $query = $this->connection->prepare("SELECT O.id, U.username, P.name, P.price FROM `order` AS O LEFT JOIN user AS U ON U.id = O.userid LEFT JOIN product AS P ON P.id = O.productid");
         } else {
             $query = $this->connection->prepare("SELECT * FROM $entityName");
         }
-
         $query->execute();
-
         return $query->fetchAll();
     }
-
     /**
      * @param $entityName
      * @param array $criteria
@@ -181,26 +148,17 @@ class EntityManager extends AbstractManager implements ConnectorInterface
     {
         $field = key($criteria);
         if (isset($criteria[$field]) && isset($criteria[$field]) !== []){
-
             if (gettype($criteria[$field]) == 'array'){
                 $value = implode(',', $criteria[$field]);
             } else {
                 $value = $criteria['user_name'];
             }
-
-            $query = $this->connection->query("SELECT * FROM $entityName WHERE $field IN ($value)");
-
+            $connection = $this->connect();
+            $query = $connection->query("SELECT * FROM $entityName WHERE $field IN ($value)");
             $query->execute();
-
             return $query->fetchAll();
-
         } else {
-
             return false;
-
         }
-
     }
-
 }
-
--- src/Layer/Connector/Connector.php
+++ src/Layer/Connector/Connector.php
@@ -1,42 +1,21 @@
 <?php
-
 namespace Layer\Connector;
-
 use Layer\Connector\ConnectorInterface;
 use PDO;
-
 class Connector implements ConnectorInterface
 {
     /**
      * @var \PDO
      */
     private $connect;
-
-    /**
-     * @var
-     */
-    private $db;
-
-    /**
-     * @var
-     */
-    private $user;
-
-    /**
-     * @var
-     */
-    private $password;
-
     /**
      * @return \PDO
      */
     public function connect()
     {
         require __DIR__ . '/../../../config/autoload.php';
-
         return new \PDO('mysql:host='. $config['host'] .';dbname='.$config['db_name'].'', $config['db_user'], $config['db_password']);
     }
-
     /**
      * @return null
      */
@@ -44,5 +23,4 @@ class Connector implements ConnectorInterface
     {
 //        return $this->connection = null;
     }
-
 }
