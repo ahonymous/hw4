f2c9b4f45127ea560c4ec74f0dc6cdae7f30a409
Roma48 <myzfann@mail.ru>
2015-11-08 15:57:31 +0200

    add prepareSql test and mock

--- src/Tests/EntityTests.php
+++ src/Tests/EntityTests.php
@@ -12,12 +12,24 @@ class EntityManagerTest extends \PHPUnit_Framework_TestCase
 
     public function testParseEntity()
     {
+        $mock = $this->getMockBuilder('Entity\EntityManager')
+            ->setMethods(array('insert'))
+            ->getMock();
+
+        $mock->method('insert')
+            ->willReturn(1);
+
         $user = new User();
         $user->setCreatedAt('1111');
         $user->setUserName('Roma');
 
         $testEntity = new EntityManager();
+
+        $this->assertInstanceOf('User\User' , $user);
+
         $parsedEntity = $testEntity->entityParse($user);
+
+        $this->assertCount(2, $parsedEntity);
 
         $this->assertArraySubset( ['entity' => 'User',
             'properties' => [
@@ -27,6 +39,27 @@ class EntityManagerTest extends \PHPUnit_Framework_TestCase
                 'updatedAt' => null,
                 'deletedAt' => null
             ]] , $parsedEntity);
+    }
+
+    public function testPrepareSql()
+    {
+        $queryArr = [
+            'entity' => 'User',
+            'properties' => [
+                'id' => null,
+                'userName' => 'Roma',
+                'createdAt' => '1111',
+                'updatedAt' => null,
+                'deletedAt' => null
+            ]
+        ];
+
+        $entityManager = new EntityManager();
+
+        $this->assertEquals("INSERT INTO user (`userName`,`createdAt`) VALUES ('Roma','1111')",
+            $entityManager->prepareSql($queryArr)
+        );
+
     }
 
 }
